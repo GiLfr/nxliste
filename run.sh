db062690a5217ce2a28da04f57795ae31a589835
Gilles L <gilles.lanteri@gmail.com>
2021-04-19 10:22:40 +0200

    upd

--- run.sh
+++ run.sh
@@ -6,15 +6,24 @@ export next=`increment_version $current`
 echo "$current --> $next"
 
 # Positionnement en development
-git branch development
+git branch develop
+
+if [ -n "$(git status --porcelain)" ]; then
+  echo "there are changes";
+else
+  echo "no changes";
+fi
 
 # Commit changes
 git status
 git add .
 git commit -m 'nvl version'
+git push origin develop
 
 # Création d'une version
 # git flow release start $next develop
 
 # Finalisation de la version
 # git flow release finish $next
+# git push origin master
+# git push origin develop
