392d8e21f2efe37802dca416c7da4c3e5da82334
Gilles L <gilles.lanteri@gmail.com>
2021-06-12 14:52:13 +0200

    Modif affichage + maj

--- run_release.sh
+++ run_release.sh
@@ -2,11 +2,12 @@ source ./next_version.sh
 
 export current=`git tag --list --sort -tag | head -n 1`
 export next=`increment_version $current`
-
 echo "$current --> $next"
 
-# Positionnement en development
-git checkout develop
+# Positionnement en development (si pas déjà le cas)
+if [ $(git rev-parse --abbrev-ref HEAD) != "develop" ]; then
+    git checkout develop
+fi
 
 # Commit si changement
 if [ -n "$(git status --porcelain)" ]; then
